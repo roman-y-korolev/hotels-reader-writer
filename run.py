0581fee01a86e2fe2429b8aecb1a7684f236c306
Roman Korolev <roman.y.korolev@gmail.com>
2018-11-05 12:33:19 +0300

    validation

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import os
 
 from config import STORAGE_PATH
 
+from validators import RatingValidator, URLValidator
+
 files = [STORAGE_PATH + file_name for file_name in os.listdir(STORAGE_PATH) if file_name.endswith('.csv')]
 
 from models.hotels import Hotels
@@ -12,4 +14,9 @@ h = Hotels(name='1', address='1', stars='1', contact='1', phone='1', uri='1')
 
 reader = CSVReader(files[0], Hotels)
 
-print(reader.mapping)
+reader.register_validator(RatingValidator)
+reader.register_validator(URLValidator)
+
+reader.read()
+
+print(reader.result)
--- validators.py
+++ validators.py
@@ -1,4 +1,37 @@
-class BaseValidator:
+from urllib.parse import urlparse
 
-    def __init__(self, fields_list):
-        self.fields_list = fields_list
+
+class BaseValidator:
+    fields = []
+
+    @classmethod
+    def check(cls, row):
+        return True
+
+
+class RatingValidator(BaseValidator):
+    fields = ['stars']
+
+    @classmethod
+    def check(cls, row):
+        try:
+            rating = int(row['stars'])
+        except ValueError:
+            return False
+
+        if rating > 5 or rating < 0:
+            return False
+
+        return True
+
+
+class URLValidator(BaseValidator):
+    fields = ['uri']
+
+    @classmethod
+    def check(cls, row):
+        parse = urlparse(row['uri'])
+        if parse.netloc == '':
+            return False
+        else:
+            return True
